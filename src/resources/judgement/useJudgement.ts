841ea33b53b4c2ece14a13f6d65ead47fff23e22
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-08-04 15:01:17 +0200

    chore(judgemenet): make composable options reactive

--- src/resources/judgement/useJudgement.ts
+++ src/resources/judgement/useJudgement.ts
@@ -1,4 +1,4 @@
-import { watch, type Ref } from "vue";
+import { computed, watch, type Ref } from "vue";
 import { CanvasAnimationFunction } from "../../composables/useCanvasAnimation";
 import { inRange, mapValues, remove } from "lodash";
 import { assert } from "../../utils/assert";
@@ -7,15 +7,8 @@ import { CanvasNote, NOTE_TYPE } from "../note/store";
 import { judgementService } from "./JudgementService";
 
 type UseJudgementOptions = {
-  SCROLL_SPEED: number;
-  COL_HEIGHT: number;
-}
-
-const calculateWindows = (scrollSpeed: number, colHeight: number) => {
-  return mapValues(JUDGEMENT_WINDOWS, (jw) => ({
-    top: colHeight - (scrollSpeed * jw) / 2,
-    bottom: colHeight + (scrollSpeed * jw) / 2
-  }))
+  SCROLL_SPEED: Ref<number>;
+  COL_HEIGHT: Ref<number>;
 }
 
 export const useJudgement = (
@@ -24,15 +17,19 @@ export const useJudgement = (
   options: UseJudgementOptions
 ) => {
   const { SCROLL_SPEED, COL_HEIGHT } = options
+
   const judged: string[] = []
-  const windows = calculateWindows(SCROLL_SPEED, COL_HEIGHT)
+  const windows = computed(() => mapValues(JUDGEMENT_WINDOWS, (jw) => ({
+    top: COL_HEIGHT.value - (SCROLL_SPEED.value * jw) / 2,
+    bottom: COL_HEIGHT.value + (SCROLL_SPEED.value * jw) / 2
+  })))
 
   // internal state
   let judgingLongNote = false
   let earlyRelease = false
 
   const drawJudgementLines: CanvasAnimationFunction = (ctx) => {
-    Object.values(windows).forEach(({ top }) => {
+    Object.values(windows.value).forEach(({ top }) => {
       ctx.fillStyle = "red"
       ctx.fillRect(0, top, 100, 1)
     })
@@ -50,7 +47,7 @@ export const useJudgement = (
 
   const onKeyPressChange = (pressed: boolean) => {
     const toBeJudged = notes.value
-      .filter(note => inRange(note.y, windows.MISS.top, windows.MISS.bottom + 1))
+      .filter(note => inRange(note.y, windows.value.MISS.top, windows.value.MISS.bottom + 1))
       .find(note => !judged.includes(note.id))
 
     if (pressed) {
@@ -73,8 +70,8 @@ export const useJudgement = (
       judgementService.add("MISS")
     } else {
       if (note.type === NOTE_TYPE.TAIL) judgingLongNote = false
-      const jw = (Object.keys(windows) as Judgement[]).find(
-        (jw) => inRange(note.y, windows[jw].top, windows[jw].bottom)
+      const jw = (Object.keys(windows.value) as Judgement[]).find(
+        (jw) => inRange(note.y, windows.value[jw].top, windows.value[jw].bottom)
       )
       assert(jw, "expected judgement window to not be undefined")
       judgementService.add(jw)
