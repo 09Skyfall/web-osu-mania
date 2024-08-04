52b0aa42d9fe3001a9c55751d6a88a8601c0c183
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-08-04 11:19:24 +0200

    chore: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
+
 import resize from './directives/resize'
 
-createApp(App).directive("resize", resize).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+app.directive("resize", resize)
+app.use(pinia)
+app.mount('#app')
