167007d706a2edcf802ed30b1fd3d1750ca6712d
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-10-03 21:11:50 +0200

    chore(utils): mapAsync

---
+++ src/utils/mapAsync.ts
@@ -0,0 +1,6 @@
+export const mapAsync = async <T, U>(
+  array: T[],
+  callbackfn: (value: T, index: number, array: T[]) => Promise<U>,
+): Promise<U[]> => {
+  return Promise.all(array.map(callbackfn));
+};
