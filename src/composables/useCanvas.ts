509116c6986d84787642ed1a442a4737e8df94ae
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-10-31 20:28:03 +0100

    chore(useCanvas): add context options

--- src/composables/useCanvas.ts
+++ src/composables/useCanvas.ts
@@ -1,8 +1,8 @@
 import { computed, ref } from "vue";
 
-export const useCanvas = () => {
+export const useCanvas = (options?: CanvasRenderingContext2DSettings) => {
   const canvas = ref<HTMLCanvasElement | null>(null);
-  const ctx = computed(() => canvas.value?.getContext("2d") ?? undefined);
+  const ctx = computed(() => canvas.value?.getContext("2d", options) ?? undefined);
 
   return { canvas, ctx };
 };
