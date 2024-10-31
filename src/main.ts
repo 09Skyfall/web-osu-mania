f2f8728b2d0c87b628db829fead169a942933792
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-10-31 20:40:12 +0100

    chore: vue router

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import resize from "./directives/resize";
+import { router } from "./plugins/router";
 
 const pinia = createPinia();
 const app = createApp(App);
 app.directive("resize", resize);
 app.use(pinia);
+app.use(router);
 app.mount("#app");
