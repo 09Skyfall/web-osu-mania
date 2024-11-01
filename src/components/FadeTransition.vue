ad565137020e007d8405f0eabca979f7113909d5
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-11-01 17:43:21 +0100

    feat(components): fade transition

---
+++ src/components/FadeTransition.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ duration?: number }>(), { duration: 250 });
+</script>
+
+<template>
+  <Transition name="fade">
+    <slot />
+  </Transition>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity v-bind("`${duration}ms`") ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
