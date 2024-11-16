c4d0ec313c231d2515d2ed62a7f49fb4fac3ca76
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2024-11-16 11:05:14 +0100

    feat(transitions): expand x transition

---
+++ src/components/ExpandXTransition.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+withDefaults(defineProps<{ duration?: number }>(), { duration: 250 });
+</script>
+
+<template>
+  <Transition name="expand-x">
+    <slot />
+  </Transition>
+</template>
+
+<style scoped>
+.expand-x-enter-active,
+.expand-x-leave-active {
+  transition: width v-bind("`${duration}ms`") ease-in-out;
+}
+
+.expand-x-enter-from,
+.expand-x-leave-to {
+  width: 0;
+}
+</style>
