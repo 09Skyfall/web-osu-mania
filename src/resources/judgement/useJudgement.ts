918d5471be9de7c2f0df5b3dbbbca9e2d98c9f11
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2025-03-08 21:16:57 +0100

    chore: remove unused import

--- src/resources/judgement/useJudgement.ts
+++ src/resources/judgement/useJudgement.ts
@@ -1,5 +1,5 @@
 import { computed, watch, type Ref } from "vue";
-import { inRange, mapValues, remove } from "lodash";
+import { inRange, mapValues } from "lodash";
 import { type Judgement, JUDGEMENT_WINDOWS } from "./store";
 import { CanvasNote, NOTE_TYPE } from "../note/store";
 import { judgementService } from "./JudgementService";
