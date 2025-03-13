a0aefef7963c584fc84fec0ab0fb6be74a091da8
Nicolò Cassiani <nicolo.cassiani@minnovi.com>
2025-03-13 21:27:34 +0100

    fix(useKey): ignore case sensitivity by default for letters

--- src/composables/useKey.ts
+++ src/composables/useKey.ts
@@ -1,23 +1,37 @@
 import { ref } from "vue";
 import { useEventListener } from "./useEventListener";
+import { isLetter } from "../utils/functions/isLetter";
 
 export type UseKeyOptions = {
   on_key_down?: (e: KeyboardEvent) => void;
   on_key_up?: (e: KeyboardEvent) => void;
+  caseSensitive?: boolean;
 };
 
-export const useKey = (key: KeyboardEvent["key"], options: UseKeyOptions = {}) => {
+export const useKey = (
+  key: KeyboardEvent["key"],
+  { on_key_down, on_key_up, caseSensitive = false }: UseKeyOptions = {},
+) => {
   const active = ref(false);
 
+  const isCorrectKey = (k: KeyboardEvent["key"]) => {
+    if (!caseSensitive && isLetter(key)) return k.toLowerCase() === key.toLowerCase();
+    else return k === key;
+  };
+
   const onKeyDown = (e: Event) => {
-    if ((e as KeyboardEvent).key !== key) return;
-    if (options.on_key_down) options.on_key_down(e as KeyboardEvent);
+    if (!isCorrectKey((e as KeyboardEvent).key)) return;
+
+    if (on_key_down) on_key_down(e as KeyboardEvent);
+
     active.value = true;
   };
 
   const onKeyUp = (e: Event) => {
-    if ((e as KeyboardEvent).key !== key) return;
-    if (options.on_key_up) options.on_key_up(e as KeyboardEvent);
+    if (!isCorrectKey((e as KeyboardEvent).key)) return;
+
+    if (on_key_up) on_key_up(e as KeyboardEvent);
+
     active.value = false;
   };
 
